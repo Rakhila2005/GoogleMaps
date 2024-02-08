acb373ec878ecdd8b01e8bf2306679127dd557c0
Rakhila2005 <146660572+Rakhila2005@users.noreply.github.com>
2024-02-08 10:26:58 +0600

    dog.js

--- public/js/dog.js
+++ public/js/dog.js
@@ -1,5 +1,5 @@
 //change the limit to however many images to use
-const url = `https://api.thecatapi.com/v1/images/search?limit=10`;
+/*const url = `https://api.thecatapi.com/v1/images/search?limit=10`;
 const api_key = "live_PiEmMwbrtaODQWc7lEGFp7J01DS47KCd6s1Ffpu6k7DgSU7J7Gcn3hYDsfWTXEcy"
 
  fetch(url,{headers: {
@@ -27,4 +27,22 @@ const api_key = "live_PiEmMwbrtaODQWc7lEGFp7J01DS47KCd6s1Ffpu6k7DgSU7J7Gcn3hYDsf
 })
 .catch(function(error) {
    console.log(error);
-});
+});*/
+
+
+// JavaScript code
+var httpRequest = new XMLHttpRequest();
+httpRequest.open("get","https://dog.ceo/api/breeds/image/random");
+httpRequest.send();
+
+httpRequest.onreadystatechange = aFunction;
+
+function aFunction() {
+    if (httpRequest.readyState == 4 && httpRequest.status == 200) {
+        var dogs = httpRequest.responseText;
+        console.log(typeof(dogs))
+        var dogsObj = JSON.parse(dogs);
+        console.log(dogsObj);
+        document.getElementById('pic').src=dogsObj.message;
+    }
+}
